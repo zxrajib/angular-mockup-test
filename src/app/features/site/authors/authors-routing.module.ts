0427fa16b7e7cae08514e9f98354083e9b8538c9
Rajib <zxrajib@gmail.com>
2022-02-06 10:52:51 +0600

    html title heading

--- src/app/features/site/authors/authors-routing.module.ts
+++ src/app/features/site/authors/authors-routing.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { FavouriteAuthorComponent } from './favourite-author/favourite-author.component';
-import { ListComponent } from './list/list.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { FavouriteAuthorComponent } from "./favourite-author/favourite-author.component";
+import { ListComponent } from "./list/list.component";
 
 const routes: Routes = [
   {
-    path: 'list',
+    path: "list",
     component: ListComponent,
-    data: { title: 'List' },
+    data: { title: "List" },
   },
   {
-    path: 'favourite-list',
+    path: "favourite-list",
     component: FavouriteAuthorComponent,
-    data: { title: 'Favourtie List' },
+    data: { title: "Favourtie List" },
   },
 ];
 
